4ccac103921d6408ea94bae978c73a89f4a68666
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-11 08:04:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,8 +7,8 @@ def deal_card
   num
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
