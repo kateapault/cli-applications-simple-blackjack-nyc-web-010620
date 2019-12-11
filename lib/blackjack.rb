ec246fa8e4630d106187a2cafd962e724592461a
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-11 08:08:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,11 +20,11 @@ def get_user_input
 end
 
 def end_game(card_total)
-  "Sorry, you hit #{card_total}. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  
 end
 
 def hit?
